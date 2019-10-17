bfcf76522a940c3afc031c2eca5e1026938cc982
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-10-17 21:43:28 +0300

    Add string constants to test file

--- src/test/java/FilesValidationTest.java
+++ src/test/java/FilesValidationTest.java
@@ -12,6 +12,14 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 
 public class FilesValidationTest {
+    private final static String NAME_OF_THE_FIRST_FILE = "test1.txt";
+    private final static String NAME_OF_THE_SECOND_FILE = "test2.log";
+    private final static String NAME_OF_THE_Folder = "TestFolder";
+    private final static String NAME_OF_THE_THIRD_FILE = "test4.txt";
+    private final static String MOCKED_RETURNING_PATH = "/anyFolder/";
+    private final static String MOCKED_RETURNING_PATH_FOR_INSIDE_FILE = "innerFolder/";
+    private final static String VALID_EXTENSION = "txt";
+
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
@@ -19,27 +27,27 @@ public class FilesValidationTest {
     @Test
     public void testFindAllValidFilesInDirectoryBreadthSearchMethod() throws Exception {
         FilesValidation filesValidation = new FilesValidation();
-        File test1 = temporaryFolder.newFile("test1.txt");
-        File test2 = temporaryFolder.newFile("test2.log");
-        File test3 = temporaryFolder.newFolder("TestFolder");
-        File test4 = temporaryFolder.newFile("test4.txt");
+        File test1 = temporaryFolder.newFile(NAME_OF_THE_FIRST_FILE);
+        File test2 = temporaryFolder.newFile(NAME_OF_THE_SECOND_FILE);
+        File test3 = temporaryFolder.newFolder(NAME_OF_THE_Folder);
+        File test4 = temporaryFolder.newFile(NAME_OF_THE_THIRD_FILE);
 
         File mockedFile = Mockito.mock(File.class);
         File mockedFileInside = Mockito.mock(File.class);
-        when(mockedFile.getPath()).thenReturn("/anyFolder/");
+        when(mockedFile.getPath()).thenReturn(MOCKED_RETURNING_PATH);
         when(mockedFile.isFile()).thenReturn(false);
         when(mockedFile.listFiles()).thenReturn(
                 new File[]{test1,
                         test2,
                         test3,
                         mockedFileInside});
-        when(mockedFileInside.getPath()).thenReturn("innerFolder/");
+        when(mockedFileInside.getPath()).thenReturn(MOCKED_RETURNING_PATH_FOR_INSIDE_FILE);
         when(mockedFileInside.isFile()).thenReturn(false);
         when(mockedFileInside.isDirectory()).thenReturn(true);
         when(mockedFileInside.listFiles()).thenReturn( new File[]{test4});
 
 
-        List<File> actual = filesValidation.findAllValidFilesInDirectoryBreadthSearch(mockedFile, "txt");
+        List<File> actual = filesValidation.findAllValidFilesInDirectoryBreadthSearch(mockedFile, VALID_EXTENSION);
         List<File> expected= Arrays.asList(new File[]{test1,test4});
         Assert.assertEquals(expected,actual);
     }
